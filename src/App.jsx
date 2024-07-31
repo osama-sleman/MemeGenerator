de4d32f0925b888a357d7d8e3d4a0430d23fb5f0
osama-sleman <osamasleman444@gmail.com>
2024-07-31 19:53:13 +0300

    new v

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,6 @@ import Header from "../src/assets/components/Header";
 import Meme from "../src/assets/components/Body";
 import "./App.css";
 
-/**
- * Challenge:
- * - Create a Meme component.
- * - Inside the Meme component, render a styled form
- *   with our 2 inputs and the button.
- * - Don't worry about adding any functionality yet
- */
 export default function App() {
   return (
     <div>
